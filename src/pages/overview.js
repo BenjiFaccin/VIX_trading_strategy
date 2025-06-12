281d44874d57f221df9fce27a7c26c13a55ffb2d
Benjamin FACCIN <benjamin.faccin06@gmail.com>
2025-06-12 17:59:38 +0200

    update website

--- src/pages/overview.js
+++ src/pages/overview.js
@@ -130,7 +130,7 @@ export default function OverviewPage() {
   );
 })()}
         {(() => {
-  const exitedFiltered = trades.filter(row => row['Status'] === 'Exited');
+  const exitedFiltered = exitData.filter(row => row['Status'] === 'Exited');
   return (
     <>
       <h1 style={{ textAlign: 'center', marginBottom: '1.5rem', fontSize: '1.8rem' }}>
